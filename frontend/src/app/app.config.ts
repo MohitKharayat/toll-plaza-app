51be72cd3f86135af490ea43fdd12e0e71da6931
Mohit Kharayat <mohitkharayat0@gmail.com>
2026-05-21 03:56:46 +0530

    Completed Toll Plaza Dashboard application

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { withFetch } from '@angular/common/http';
 
 import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
 providers: [
   provideRouter(routes),
-  provideHttpClient()
+  provideHttpClient(withFetch())
 ]};
